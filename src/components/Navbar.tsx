9507b0fbbdc596478f4009d7fce85b62839a3da7
Fabricio C <fabriciocesaretti@gmail.com>
2022-08-02 08:31:39 -0300

    + click event hambuerger menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,30 +4,30 @@ export const Navbar = () => {
 	return (
 		<nav className='relative flex flex-wrap items-center justify-between px-2 py-3 bg-slate-900 mb-3'>
 			<div className='container px-4 mx-auto flex flex-wrap items-center justify-between'>
-				<div className='w-full relative flex justify-between lg:w-auto  px-4 lg:static lg:block lg:justify-start'>
+				<div className='w-full relative flex justify-between md:w-auto  px-4 md:static md:block md:justify-start'>
 					<NavLink
-						className='text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white'
+						className='text-xl font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white'
 						to='/'
 					>
 						Search APP
 					</NavLink>
 					<button
-						className='cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none'
+						className='cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block md:hidden outline-none focus:outline-none'
 						type='button'
+						onClick={() => {
+							document.getElementById('menu')?.classList.toggle('hidden');
+						}}
 					>
 						<span className='block relative w-6 h-px rounded-sm bg-white'></span>
 						<span className='block relative w-6 h-px rounded-sm bg-white mt-1'></span>
 						<span className='block relative w-6 h-px rounded-sm bg-white mt-1'></span>
 					</button>
 				</div>
-				<div
-					className='lg:flex flex-grow items-center hidden'
-					id='example-navbar-warning'
-				>
-					<ul className='flex flex-col lg:flex-row list-none ml-auto'>
+				<div className='hidden md:flex flex-grow items-center' id='menu'>
+					<ul className='text-xl md:text-sm flex items-center flex-col md:flex-row list-none md:ml-auto'>
 						<li className='nav-item'>
 							<NavLink
-								className='px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75'
+								className='px-3 py-2 flex items-center uppercase font-bold leading-snug text-white hover:opacity-75'
 								to='/rickandmorty'
 							>
 								Rick And Morty
@@ -35,7 +35,7 @@ export const Navbar = () => {
 						</li>
 						<li className='nav-item'>
 							<NavLink
-								className='px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75'
+								className='px-3 py-2 flex items-center uppercase font-bold leading-snug text-white hover:opacity-75'
 								to='/simpsons'
 							>
 								The Simpsons
@@ -43,7 +43,7 @@ export const Navbar = () => {
 						</li>
 						<li className='nav-item'>
 							<NavLink
-								className='px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75'
+								className='px-3 py-2 flex items-center uppercase font-bold leading-snug text-white hover:opacity-75'
 								to='/futurama'
 							>
 								Futurama
@@ -51,7 +51,7 @@ export const Navbar = () => {
 						</li>
 						<li className='nav-item'>
 							<NavLink
-								className='px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75'
+								className='px-3 py-2 flex items-center uppercase font-bold leading-snug text-white hover:opacity-75'
 								to='/cartoons'
 							>
 								Cartoons
