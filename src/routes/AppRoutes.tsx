365ddc36e4d1d96ec8474f6fd4000abc121913d9
Fabricio C <fabriciocesaretti@gmail.com>
2022-08-05 18:59:38 -0300

    new component import & new route

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,16 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
+import { CharacterDetails } from '../components/CharacterDetails';
 import { Home } from '../components/Home';
 import { NotFound } from '../components/NotFound';
-// import { SearchResult } from '../components/SearchResult';
 import { SearchScreen } from '../components/SearchScreen';
 
 export const AppRoutes = () => {
 	return (
 		<Routes>
 			<Route path='/' element={<Home />} />
-			<Route path='/:show' element={<SearchScreen />}>
-				{/* <Route path=':character' element={<SearchResult />} /> */}
-			</Route>
+			<Route path='/:show' element={<SearchScreen />} />
+			<Route path='/:show/:characterID' element={<CharacterDetails />} />
 			<Route path='/*' element={<NotFound />} />
 		</Routes>
 	);
